d4b886eba0de57ef26336816571552f59c7a432a
Wesss <wesleymcox@gmail.com>
2019-03-27 15:08:46 -0700

    main game logic

--- src/app/Board.js
+++ src/app/Board.js
@@ -13,14 +13,83 @@ const EMPTY_BOARD = [
 
 class Board extends Component {
 
+  // return the current game state of the given board
   static getGameState(board) {
-    return Game.GAME_STATE.X_MOVE;
+    var result = false;
+
+    // check for vertical and horizontal wins
+    for (var i = 0; i < 3; i++) {
+      result = Board.checkForWin(board[i][0], board[i][1], board[i][2]);
+      if (result) return result;
+      result = Board.checkForWin(board[0][i], board[1][i], board[2][i]);
+      if (result) return result;
+    }
+
+    // check for diagonal wins
+    result = Board.checkForWin(board[0][0], board[1][1], board[2][2]);
+    if (result) return result;
+    result = Board.checkForWin(board[0][2], board[1][1], board[2][0]);
+    if (result) return result;
+
+    // check for tie
+    var isTie = true;
+    _.forEach(board, (column) => {
+      _.forEach(column, (tile) => {
+        if (tile === Tile.TILE_STATE.EMPTY) {
+          isTie = false;
+        }
+      });
+    });
+    if (isTie) {
+      return Game.GAME_STATE.TIE;
+    }
+
+    // find player turn
+    var x_count = 0;
+    var o_count = 0;
+    _.forEach(board, (column) => {
+      _.forEach(column, (tile) => {
+        if (tile === Tile.TILE_STATE.X) {
+          x_count++;
+        } else if (tile === Tile.TILE_STATE.O) {
+          o_count++;
+        }
+      });
+    });
+    if (x_count > o_count) {
+      return Game.GAME_STATE.O_MOVE;
+    } else {
+      return Game.GAME_STATE.X_MOVE;
+    }
   }
 
-  static getTileStateChange(oldTile, gameState) {
-    return Tile.TILE_STATE.X;
+  // return a winning game state if three given tiles are identical player markers
+  // otherwise return false
+  static checkForWin(tile1, tile2, tile3) {
+    if (tile1 === Tile.TILE_STATE.X && tile2 === Tile.TILE_STATE.X && tile3 === Tile.TILE_STATE.X) {
+      return Game.GAME_STATE.X_WIN;
+    } else if (tile1 === Tile.TILE_STATE.O && tile2 === Tile.TILE_STATE.O && tile3 === Tile.TILE_STATE.O) {
+      return Game.GAME_STATE.O_WIN;
+    } else {
+      return false;
+    }
   }
-  
+
+  // return the next state of a clicked tile given its current state and the current game state
+  static getTileStateChange(curTile, gameState) {
+    if (curTile != Tile.TILE_STATE.EMPTY) {
+      return curTile;
+    }
+    switch (gameState) {
+      case Game.GAME_STATE.X_MOVE:
+        return Tile.TILE_STATE.X;
+      case Game.GAME_STATE.O_MOVE:
+        return Tile.TILE_STATE.O;
+      default:
+        return curTile;
+    }
+  }
+
   /*
    * While it is more intuitive for each Tile component to be aware of its own state,
    * In react it is recommended that state be lifted up to the highest component
@@ -41,8 +110,12 @@ class Board extends Component {
     return () => {
       this.setState((state, props) => {
         var board = state.board;
+        console.log(board);
         board[x][y] = Board.getTileStateChange(board[x][y], this.props.gameState);
-        props.setGameState(Board.getGameState(this.state.board));
+        console.log(board);
+        var tmp = Board.getGameState(this.state.board);
+        console.log(tmp);
+        props.setGameState(tmp);
         return {board: board};
       });
     }
--- test/app/Board.test.js
+++ test/app/Board.test.js
@@ -24,3 +24,86 @@ test("clicking a tile places the current player's shape there and advances the g
   expect(component.find(Tile).at(0).props().contents).toBe(Tile.TILE_STATE.X);
   expect(setGameStateStub.calledWith(Game.GAME_STATE.O_MOVE)).toBe(true);
 });
+
+test("clicking an empty tile fills it with the current player's marker", () => {
+  var newTileState = Board.getTileStateChange(Tile.TILE_STATE.EMPTY, Game.GAME_STATE.X_MOVE);
+  expect(newTileState).toBe(Tile.TILE_STATE.X);
+});
+
+test("clicking a filled in tile does nothing", () => {
+  var newTileState = Board.getTileStateChange(Tile.TILE_STATE.O, Game.GAME_STATE.X_MOVE);
+  expect(newTileState).toBe(Tile.TILE_STATE.O);
+});
+
+test("clicking a tile after the game  is over does nothing", () => {
+  var newTileState = Board.getTileStateChange(Tile.TILE_STATE.O, Game.GAME_STATE.TIE);
+  expect(newTileState).toBe(Tile.TILE_STATE.O);
+});
+
+test("an empty board means its X's turn", () => {
+  var gameState = Board.getGameState([
+    [Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.EMPTY],
+    [Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.EMPTY],
+    [Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.EMPTY]
+  ]);
+  expect(gameState).toBe(Game.GAME_STATE.X_MOVE);
+});
+
+test("having one more X and O means its O's turn", () => {
+  var gameState = Board.getGameState([
+    [Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.EMPTY],
+    [Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.X, Tile.TILE_STATE.EMPTY],
+    [Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.EMPTY]
+  ]);
+  expect(gameState).toBe(Game.GAME_STATE.O_MOVE);
+});
+
+test("having equal X's and O's means its X's turn", () => {
+  var gameState = Board.getGameState([
+    [Tile.TILE_STATE.O, Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.EMPTY],
+    [Tile.TILE_STATE.O, Tile.TILE_STATE.X, Tile.TILE_STATE.X],
+    [Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.EMPTY]
+  ]);
+  expect(gameState).toBe(Game.GAME_STATE.X_MOVE);
+});
+
+test("having three X's vertically adjacent means X wins", () => {
+  /*
+   * note: this renders flipped across the x=y line as it appears written in code,
+   * as we define the first array dereference to be the x position and the second
+   * to be the y position
+   */
+  var gameState = Board.getGameState([
+    [Tile.TILE_STATE.O, Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.EMPTY],
+    [Tile.TILE_STATE.X, Tile.TILE_STATE.X, Tile.TILE_STATE.X],
+    [Tile.TILE_STATE.O, Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.EMPTY]
+  ]);
+  expect(gameState).toBe(Game.GAME_STATE.X_WIN);
+});
+
+test("having three O's horizontally adjacent means O wins", () => {
+  var gameState = Board.getGameState([
+    [Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.O, Tile.TILE_STATE.X],
+    [Tile.TILE_STATE.X, Tile.TILE_STATE.O, Tile.TILE_STATE.EMPTY],
+    [Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.O, Tile.TILE_STATE.X]
+  ]);
+  expect(gameState).toBe(Game.GAME_STATE.O_WIN);
+});
+
+test("having three X's on the diagonal means X wins", () => {
+  var gameState = Board.getGameState([
+    [Tile.TILE_STATE.X, Tile.TILE_STATE.O, Tile.TILE_STATE.EMPTY],
+    [Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.X, Tile.TILE_STATE.EMPTY],
+    [Tile.TILE_STATE.EMPTY, Tile.TILE_STATE.O, Tile.TILE_STATE.X]
+  ]);
+  expect(gameState).toBe(Game.GAME_STATE.X_WIN);
+});
+
+test("having every space filled without three identical pieces in a row gives a tie", () => {
+  var gameState = Board.getGameState([
+    [Tile.TILE_STATE.X, Tile.TILE_STATE.X, Tile.TILE_STATE.O],
+    [Tile.TILE_STATE.O, Tile.TILE_STATE.X, Tile.TILE_STATE.X],
+    [Tile.TILE_STATE.X, Tile.TILE_STATE.O, Tile.TILE_STATE.O]
+  ]);
+  expect(gameState).toBe(Game.GAME_STATE.TIE);
+});
